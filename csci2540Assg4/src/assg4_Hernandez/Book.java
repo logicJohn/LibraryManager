ef1ab78ec0c9c215c6cacd4f402afa3fb7ffb79e
mr.jay <johnnie.a.hernandez@gmail.com>
2018-02-15 10:15:34 -0500

    added some basic methods for Book

--- csci2540Assg4/src/assg4_Hernandez/Book.java
+++ csci2540Assg4/src/assg4_Hernandez/Book.java
@@ -10,6 +10,7 @@ public class Book {
 	
 	private int catalogLength;
 	
+	
 	public Book () {
 		this.id = 10000;
 		this.title = "default";
@@ -17,6 +18,7 @@ public class Book {
 		this.author = "default";
 		this.category = 'F';
 	}
+	
 	
 	public Book (int id, String title, String isbn, String author, char category) {
 		this.id = id;
@@ -26,5 +28,72 @@ public class Book {
 		this.category = category;
 	}
 	
+	/**
+	 * Get the Id of book.
+	 * @return Book Id in integer form
+	 */
+	public int getId () {
+		return this.id;
+	}
+	
+	/**
+	 * Get the title of book
+	 * @return Book title as String
+	 */
+	public String getTitle () {
+		return this.title;
+	}
+	
+	/**
+	 * Get the isbn of book
+	 * @return Book ISBN as String
+	 */
+	public String getIsbn () {
+		return this.isbn;
+	}
+	
+	/**
+	 * Get the Author of the book
+	 * @return Book Author as String
+	 */
+	public String getAuthor () {
+		return this.author;
+	}
+	
+	/**
+	 * Get the category of book.  Categories are only 'F' for fiction
+	 * and 'N' for non fiction
+	 * @return Book category in the form of char
+	 */
+	public char getCategory () {
+		return this.category;
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public String toString () {
+		return " " + this.id + " " + this.title + " "
+				+ this.isbn + " " + this.author + " " 
+				+ this.category + "\n";
+	}
+	
+	/**
+	 * 
+	 * @param array
+	 * @param arraySize
+	 * @param id
+	 */
+	public static void bookSearch (Book[] array, int arraySize, int id) {
+		for (int i = 0; i < arraySize; i++) {
+			if (array[i].id==id) {
+				System.out.print(array[i].toString());
+				return;
+			}
+		}
+		System.out.println("put exception here");
+		return;
+	}
 	
 }
